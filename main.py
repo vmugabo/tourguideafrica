9bd45726324ca53523d3419dde649fd3a6fcb474
Vincent Mugabo <63720396+vmugabo@users.noreply.github.com>
2024-03-25 12:16:28 +0200

    main function asking for interests

---
+++ main.py
@@ -0,0 +1,14 @@
+from attractions import attractions
+
+
+def ask_interests():
+    print("What are your interests? (Choose one or more)")
+  
+    print("1. Wildlife")
+    print("2. Nature")
+    print("3. Hiking")
+    print("4. Scenery")
+    print("5. Water activities")
+    print("6. Relaxation")
+    print("7. History")
+    print("8. Culture")
