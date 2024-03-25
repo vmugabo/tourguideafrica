a2c270988d081a1e309c6d1f719f02c9608254d2
Ninette03 <n.agatesi@alustudent.com>
2024-03-25 02:53:19 -0700

    More tourist attractions

--- attractions.py
+++ attractions.py
@@ -4,6 +4,7 @@ attractions = {
     "3": {"name": "Lake Kivu", "description": "A beautiful lake with beaches, boat tours, and water activities.", "interests": ["scenery", "water activities", "relaxation"]},
     "4": {"name": "Kigali Genocide Memorial", "description": "A museum commemorating the 1994 genocide against the Tutsi.", "interests": ["history", "culture"]},
     "5": {"name": "Akagera National Park", "description": "A safari park with a variety of wildlife including elephants, lions, and rhinos.", "interests": ["wildlife", "safari", "nature"]}
+    "6": {"name": "King's palace Museum", "description": "A reconstruction of the traditional royal residence, the King’s Palace is a beautifully-crafted thatched dwelling shaped like a beehive", "interests": ["history", "culture"]}
 }
 
 print(attractions["3"])
