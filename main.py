1a4eb0ab407013e9d2a9002beb0fdd6a559a596f
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2024-04-01 11:43:24 +0200

    add: infinite loop of using the application

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from attractions import attractions
 
+# Constants
+APP_NAME = "Tourist Attractions Recommendation System"
+
+interest_mapping = {
+    '1': 'wildlife',
+    '2': 'nature',
+    '3': 'hiking',
+    '4': 'scenery',
+    '5': 'water activities',
+    '6': 'relaxation',
+    '7': 'history',
+    '8': 'culture',
+    '9': 'art',
+    '10': 'night life',
+    '11': 'urbanization',
+}
+
 
 def recommend_attractions(interests):
     recommended_attractions = []
@@ -17,6 +34,11 @@ def recommend_attractions(interests):
     return recommended_attractions
 
 
+def exitTheApplication():
+    print(f'Thanks for using the ${APP_NAME} app!')
+    return exit(1)
+
+
 def ask_interests():
     print("What are your interests? (Choose one or more)")
     print()
@@ -31,48 +53,45 @@ def ask_interests():
     print("9. Art")
     print("10. Night Life")
     print("11. Urbanization")
+
+    print("To exit the application press -1")
     print()
     interests_input = input(
         "Enter the numbers corresponding to your interests, separated by commas: ")
+
+    if interests_input == "-1":
+        exitTheApplication()
     interests = [interest_mapping.get(interest.strip())
                  for interest in interests_input.split(',')]
     return [interest for interest in interests if interest]
 
-interest_mapping = {
-    '1': 'wildlife',
-    '2': 'nature',
-    '3': 'hiking',
-    '4': 'scenery',
-    '5': 'water activities',
-    '6': 'relaxation',
-    '7': 'history',
-    '8': 'culture',
-    '9': 'art',
-    '10': 'night life',
-    '11': 'urbanization',
-}
 
-
-print("=========== Welcome to the Rwanda Tourist Attractions Recommendation System! ===========")
+print("=========== Welcome to the Rwanda " + APP_NAME + "! ===========")
 print()
 print("================ Here are some popular tourist attractions in Rwanda: ==================")
 print()
-selected_interests = ask_interests()
 
-# Creating a new dictionary that contains any of the selected interests variable
+while True:
+    selected_interests = ask_interests()
 
-filtered_data = {key: value for key, value in attractions.items() if any(interest in value.get('interests', []) for interest in selected_interests)}
+    # Creating a new dictionary that contains any of the selected interests variable
 
-if filtered_data:
-    print("\nHere are the attractions matching your interests:")
-    recommended_attractions = recommend_attractions(selected_interests)
-    for key, value in filtered_data.items():
-        print('Name: {},'.format(value['name']))
-        print('Description: {},'.format(value['description']))
-        print('Location: {}, {},'.format(value['district'], value['province']))
-        print('Interests: {},'.format(', '.join(value['interests'])))
-        print('-' * 80)
+    filtered_data = {key: value for key, value in attractions.items() if
+                     any(interest in value.get('interests', []) for interest in selected_interests)}
 
-else:
-    print("Sorry, there are no attractions matching your interests.")
+    if filtered_data:
+        print("\nHere are the attractions matching your interests:")
+        recommended_attractions = recommend_attractions(selected_interests)
+        for key, value in filtered_data.items():
+            print('Name: {},'.format(value['name']))
+            print('Description: {},'.format(value['description']))
+            print('Location: {}, {},'.format(value['district'], value['province']))
+            print('Interests: {},'.format(', '.join(value['interests'])))
+            print('-' * 80)
 
+    else:
+        print("Sorry, there are no attractions matching your interests.")
+
+    next_action = input("Press any key to continue and -1 to exit")
+    if next_action == "-1":
+        exitTheApplication()
